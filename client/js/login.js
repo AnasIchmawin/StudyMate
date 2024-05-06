6855491fccd2e4c4ebd4e9df4174329102f10e86
ali harit <mike@magic.org>
2024-05-06 15:31:04 +0100

    create project laravel , install bootstrap

--- client/js/login.js
+++
@@ -1,9 +0,0 @@
- // JavaScript pour ajouter une classe au bluescreen lors du clic sur le lien Sign Up
- document.getElementById('signup-link').addEventListener('click', function () {
-    document.querySelector('.bluescreen').classList.add('translate-right');
-});
-
-// JavaScript pour retirer la classe bluescreen lors du clic sur le lien Sign In
-document.getElementById('signin-link').addEventListener('click', function () {
-    document.querySelector('.bluescreen').classList.remove('translate-right');
-});
