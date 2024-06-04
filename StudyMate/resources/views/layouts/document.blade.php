eaa957249be7c7894752b33921e866ef16848a21
ali harit <mike@magic.org>
2024-06-04 16:50:03 +0100

    ss

--- StudyMate/resources/views/layouts/document.blade.php
+++ StudyMate/resources/views/layouts/document.blade.php
@@ -30,12 +30,25 @@
         </div>
         <div id="modules">
             @foreach($documents as $doc)
-            <div class="card">
+            <!-- <div class="card">
                 <h3 class="card__title">{{ $doc->module_id }}</h3>
                 <div class="card__date">{{ $doc->path }}</div>
+            </div> -->
+
+            @endforeach
+
+        </div>
+        <div id="modules">
+            @foreach($documents as $doc)
+            <div class="card">
+                <a href="{{ url($doc->path) }}" class="card__content">
+                    <h3 class="card__title">{{ $doc->module_id }}</h3>
+                    <div class="card__date">{{ $doc->path }}</div>
+                </a>
             </div>
             @endforeach
         </div>
+
     </div>
 
 
--- StudyMate/resources/views/layouts/module.blade.php
+++ StudyMate/resources/views/layouts/module.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Study Mate</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/module.css">
-    <link rel="stylesheet" href="css/module2.css">
+    <link rel="stylesheet" href="{{asset('css/module.css')}}">
+    <link rel="stylesheet" href="{{asset('css/module2.css')}}">
 </head>
 
 <body>
@@ -48,27 +48,27 @@
     <div id="mainContainer">
         <div class="containerNav">
             <div class="titlePage">Sessions</div>
-            <a href="{{ route('modules.create'),['id' => $module->id] }}" class="btn">Ajouter Module</a>
-        </div>
-
-        <div id="modules">
-            @foreach($modules as $module)
-            <div class="card">
-                <a href="{{ route('modules.show',['id' => $module->id]) }}">
-                    <h3 class="card__title">{{ $module->module }}</h3>
-                    <div class="card__date">{{ $module->created_at->format('F j, Y') }}</div>
-                    <div class="card__arrow">
-
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="15" width="15">
-                            <path fill="#fff" d="M13.4697 17.9697C13.1768 18.2626 13.1768 18.7374 13.4697 19.0303C13.7626 19.3232 14.2374 19.3232 14.5303 19.0303L20.3232 13.2374C21.0066 12.554 21.0066 11.446 20.3232 10.7626L14.5303 4.96967C14.2374 4.67678 13.7626 4.67678 13.4697 4.96967C13.1768 5.26256 13.1768 5.73744 13.4697 6.03033L18.6893 11.25H4C3.58579 11.25 3.25 11.5858 3.25 12C3.25 12.4142 3.58579 12.75 4 12.75H18.6893L13.4697 17.9697Z">
-                            </path>
-                        </svg>
-                    </div>
-                </a>
-            </div>
-            @endforeach
-        </div>
+            <a href="{{ route('modules.create') }}" class="btn">Ajouter Module</a>
     </div>
-</body>
+
+    <div id="modules">
+        @foreach($modules as $module)
+        <div class="card">
+            <a href="{{ route('modules.show',['id' => $module->id]) }}">
+                <h3 class="card__title">{{ $module->module }}</h3>
+                <div class="card__date">{{ $module->created_at->format('F j, Y') }}</div>
+                <div class="card__arrow">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="15" width="15">
+                        <path fill="#fff" d="M13.4697 17.9697C13.1768 18.2626 13.1768 18.7374 13.4697 19.0303C13.7626 19.3232 14.2374 19.3232 14.5303 19.0303L20.3232 13.2374C21.0066 12.554 21.0066 11.446 20.3232 10.7626L14.5303 4.96967C14.2374 4.67678 13.7626 4.67678 13.4697 4.96967C13.1768 5.26256 13.1768 5.73744 13.4697 6.03033L18.6893 11.25H4C3.58579 11.25 3.25 11.5858 3.25 12C3.25 12.4142 3.58579 12.75 4 12.75H18.6893L13.4697 17.9697Z">
+                        </path>
+                    </svg>
+                </div>
+            </a>
+        </div>
+        @endforeach
+    </div>
+    </div>
+    </body>
 
 </html>
